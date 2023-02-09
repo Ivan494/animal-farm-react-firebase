564cfe6ed4e383ec9f653243be067752aabdfcab
OPHaru <wuchitho1@hotmail.com>
2023-02-09 09:08:01 +0800

    fix urlsearchparams, end

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
+  const [animals, setAnimals] = useState([]);
 
-  const [animals,setAnimals]=useState([]);
-
-  const search = async (q) =>
-{
-  const response = await fetch(
-    'http://localhost:8080?' + new URLSearchParams({ q })
-  );
-  const data = await response.json();
-  setAnimals(data);
-}
+  const search = async (q) => {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+  };
 
   return (
     <main>
       <h1>Animal Farm</h1>
 
-      <input type="text" placeholder="Search" onChange={(e)=>search(e.target.value)} />
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
 
       <ul>
-        {animals.map((animal)=>(
+        {animals.map((animal) => (
           <Animal key={animal.id} {...animal} />
         ))}
 
-        {animals.length ===0&& 'No animals found'}
+        {animals.length === 0 && "No animals found"}
       </ul>
-
     </main>
-  )
+  );
 }
 
-function Animal({type,name,age}){
+function Animal({ type, name, age }) {
   return (
     <li>
       <strong>{type}</strong> {name} {age}
-      </li>
-  )
+    </li>
+  );
 }
 
-export default App
+export default App;
